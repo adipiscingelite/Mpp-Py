347c084fc2f0afe0d6c1f11962fefd404e463c6e
adipiscingelite <syaifulrahmatdani.18>
2024-02-18 01:42:31 +0700

    edit views.py dan urls.py

---
+++ urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+
+app_name = 'app_name_pertama'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    # path('/', views., name=''),
+    path('home/', views.home, name='home'),
+]
+
+app_name = 'app_name_kedua'
+
+urlpatterns = [
+    path('home/', views.home, name='home'),
+]
