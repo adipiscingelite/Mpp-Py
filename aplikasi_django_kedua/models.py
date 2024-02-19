efc477928c30d04f86e3dab865461f64c6534df7
adipiscingelite <syaifulrahmatdani.18>
2024-02-19 09:09:11 +0700

    all(), first(), last()

--- aplikasi_django_kedua/models.py
+++ aplikasi_django_kedua/models.py
@@ -3,3 +3,8 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=10, decimal_places=2)
+
+class Coba(models.Model):
+    nama = models.CharField(max_length=255)
+    usia = models.DecimalField(max_digits=10, decimal_places=2)
+
--- aplikasi_django_kedua/views.py
+++ aplikasi_django_kedua/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from .models import Product
+from .models import Product, Coba
 
 def tes(request):
-    products = Product.objects.first()
-    return render(request, 'tes.html', {'products': products})
+    products = Product.objects.all()
+    coba = Coba.objects.first()
+    last = Coba.objects.last()
+    return render(request, 'tes.html', {'products': products, 'coba': coba, 'last': last})
